67b696fb7c6e3de4d7c88673a74c68d4028115f0
olivier5741 <olivier5741@gmail.com>
2021-08-26 17:52:57 +0200

    clasp and google workspace app set up

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,8 @@ import * as m1 from './module1'
 function start(){// eslint-disable-line
   return m1.test1()
 }
+
+function buildHomePage() : GoogleAppsScript.Card_Service.Card {// eslint-disable-line
+  return CardService.newCardBuilder()
+    .setHeader(CardService.newCardHeader().setTitle("test1")).build()
+}
